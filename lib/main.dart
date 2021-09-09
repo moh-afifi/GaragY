e5ff589979527a1311053c3cede03d970a4a7546
moh-afifi <moh.afifi93@gmail.com>
2021-09-09 13:17:19 +0200

    master changes main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
+//-------------------------------------------------------------
 import 'package:flutter/material.dart';
 import 'package:mygarage/screens/login_screen.dart';
 import 'package:provider/provider.dart';
 import 'provider/garage_provider.dart';
-
+//-------------------------------------------------------------
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
